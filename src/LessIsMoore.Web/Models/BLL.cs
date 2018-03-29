41c72faab63d0c54213e968c711f83ccc8bcd1ee
Tim Moore <bham2052000@yahoo.com>
2018-03-29 11:12:27 -0500

    Added langusge check config

--- src/LessIsMoore.Web/Models/BLL.cs
+++ src/LessIsMoore.Web/Models/BLL.cs
@@ -33,6 +33,8 @@ namespace LessIsMoore.Web.Models
         public Models.TranslatorSettings TranslatorSettings { get; set; }
         public string NotificationHubConn { get; set; }
         public string NotificationHubName { get; set; }
+        public bool FeatureFlag_ShowLanguageKlingon { get; set; }
+
         public string VSTSToken { get; set; }
     }
 
